c66dd6f55c29b925b57fc9e56830e86fa0d0ecbf
Keith Buckholz <keith.buckholz@yale.edu>
2025-05-16 16:55:02 -0400

    added git sha versioning

--- py/psf_grid_utils.py
+++ py/psf_grid_utils.py
@@ -1,5 +1,7 @@
 import os
 import webbpsf
+import subprocess
+from astropy.io import fits
 
 github_dir_env=os.getenv('github_dir')
 if github_dir_env is None:
@@ -23,18 +25,43 @@ def load_psf_grid(grid_file, github_dir=github_dir_env):
     grid = webbpsf.utils.to_griddedpsfmodel(filepath)
     return grid
 
-def save_grid_all_detectors(wavelength, fov_pixels=364, outdir="psf_grids/data", github_dir=github_dir_env, **kwargs):
+def save_grid_all_detectors(wavelength, outdir, fov_pixels=364, **kwargs):
     """
     Computes and saves GriddedPSFModel fits files for all detectors at a given wavelength. 
     Follows naming scheme: 
     {instrument}_{filter}_{fovp}_{wavelength}_{det}.fits
     e.g. wfi_grism0_fovp364_10000_sca01.fits
     """
-    save_loc = os.path.join(github_dir, outdir)
+
     outfile = f"{wfi.name}_{wfi.filter}_fovp{fov_pixels}_{wavelength}".lower() #_det.fits is added automatically
 
     wfi.psf_grid(all_detectors=True, use_detsampled_psf=True, monochromatic=wavelength*1e-10, fov_pixels=fov_pixels, 
-                save=True, outdir=save_loc, outfile=outfile, overwrite=True, **kwargs)
+                save=True, outdir=outdir, outfile=outfile, overwrite=True, **kwargs)
     
     return 0
 
+def add_git_sha_heder(filepath):
+
+    gitsha = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+
+    file = fits.open(filepath)
+    
+    file[0].header["gitsha"] = gitsha
+    file.writeto(filepath, overwrite=True)
+    file.close()
+
+    return 0
+
+def check_version(filepath):
+
+    gitsha = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+
+    file = fits.open(filepath)
+
+    try:
+        if file[0].header["gitsha"] == gitsha:
+            return 0
+        else:
+            return 1
+    except KeyError:
+        return 2
