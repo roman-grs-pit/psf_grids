96a2b276ca6d01e73365cf2b8c47958f05397f23
Keith Buckholz <keith.buckholz@yale.edu>
2025-05-23 19:39:08 -0400

    updated job runtime and script location

--- scripts/psf_job_script.sh
+++ scripts/psf_job_script.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=keith.buckholz@yale.edu
 #SBATCH --mail-type=ALL
 #SBATCH -A m4943
-#SBATCH -t 0:90:0
+#SBATCH -t 0:45:0
 #SBATCH -L cfs
 
-srun -n 1 -c 90 --cpu_bind=cores python /global/cfs/cdirs/m4943/grismsim/psf_grids/scripts/mk_all_grids.py
+srun python /global/common/software/m4943/grizli0/psf_grids/scripts/mk_all_grids.py
